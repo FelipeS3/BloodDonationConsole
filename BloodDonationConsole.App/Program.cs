5517b3032e0a3c60c763e85348b8d3027029513a
FelipeS3 <Felipexy50@gmail.com>
2025-03-12 16:58:16 -0300

    firstCommit

---
+++ BloodDonationConsole.App/Program.cs
@@ -0,0 +1,235 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace BancoDoacaoDeSangue.App
+{
+
+    class Program
+    {
+        static List<Doador> doadores = new List<Doador>();
+        static List<Doacao> doacoes = new List<Doacao>();
+        static List<EstoqueDeSangue> estoquesDeSangue = new List<EstoqueDeSangue>();
+
+        private static int DoadorId = 1;
+        private static int DoacoesId = 1;
+        private static int EstoqueId = 1;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bem vindo ao programa.");
+                Console.WriteLine("Escolha uma opção");
+                Console.WriteLine("1 - Cadastrar  Doadores");
+                Console.WriteLine("2 - Registrar doação");
+                Console.WriteLine("3 - Consultar doador");
+                Console.WriteLine("4 - Sair");
+
+                if (!int.TryParse(Console.ReadLine(), out int opcao))
+                {
+                    Console.WriteLine("Opção inválida.");
+                    continue;
+                }
+
+                switch (opcao)
+                {
+                    case 1: CadastrarDoador(); break;
+                    case 2: RegistrarDoacao(); break;
+                    case 3: ConsultarDoador(); break;
+                    case 4: Environment.Exit(0); break;
+                }
+            }
+
+        }
+
+        static void CadastrarDoador()
+        {
+            Console.WriteLine("Por favor informe seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Insira seu Email: ");
+            string email = Console.ReadLine();
+            if (doadores.Any(d => d.Email == email))
+            {
+                Console.WriteLine("Já existe um doador com esse email");
+                return;
+            }
+
+            Console.WriteLine("Data de Nascimento: (dd/mm/aaaa)");
+            if(!DateTime.TryParse(Console.ReadLine(), out DateTime dataNascimento))
+            {
+                Console.WriteLine("Data em formato inválido.");
+            }
+
+            Console.WriteLine("Informe seu gênero: (M/F)");
+            string genero = Console.ReadLine();
+
+            Console.WriteLine("Informe seu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+            if (peso < 50)
+            {
+                Console.WriteLine("Peso inválido, mínimo de 50kg.");
+                return;
+            }
+
+            Console.WriteLine("Informe seu tipo sanguineo");
+            string tipoSanguineo = Console.ReadLine();
+
+            Console.WriteLine("Fator RH: ");
+            string fatorRH = Console.ReadLine();
+
+            var doador = new Doador
+            {
+                Id = DoadorId++,
+                Nome = nome,
+                Email = email,
+                DataNascimento = dataNascimento,
+                Genero = genero,
+                Peso = peso,
+                TipoSanguineo = tipoSanguineo,
+                FatorRh = fatorRH,
+                Doacoes = new List<Doacao>()
+            };
+
+            doadores.Add(doador);
+            Console.WriteLine("Doador cadastrado com sucesso!");
+        }
+
+        static void RegistrarDoacao()
+        {
+            Console.WriteLine("Insira seu email: ");
+            string email = Console.ReadLine();
+            var doador = doadores.FirstOrDefault(x => x.Email == email);
+
+            if (doador is null)
+            {
+                Console.WriteLine("Doador não encontrado");
+                return;
+            }
+            
+            Console.WriteLine("Quantidade de ML doada: ");
+            if (!int.TryParse(Console.ReadLine(), out int quantidadeML))
+            {
+                Console.WriteLine("Erro: ao adicionar Ml");
+                return;
+            }
+
+            if (quantidadeML < 420 || quantidadeML > 470)
+            {
+                Console.WriteLine("Quantidade deve estar entre 420 e 470.");
+                return;
+            }
+
+            var ultimaDoacao = doador.Doacoes.OrderByDescending(x => x.DataDoacao).FirstOrDefault();
+
+            if(ultimaDoacao != null)
+            {
+                TimeSpan data = DateTime.Now - ultimaDoacao.DataDoacao;
+                if (doador.Genero == "F" && data.TotalDays < 90)
+                {
+                    Console.WriteLine("Doadoras só poderão doar com um intervalo de 90 dias");
+                    return;
+                }
+                else if(doador.Genero == "M" && data.TotalDays < 60)
+                {
+                    Console.WriteLine("Doadores só poderão doar com um intervalo de 60 dias");
+                    return;
+                }
+            }
+
+            var doacao = new Doacao
+            {
+                Id = DoacoesId++,
+                IdDoador = doador.Id,
+                DataDoacao = DateTime.Now,
+                QuantidadeMl = quantidadeML
+            };
+            
+            doador.Doacoes.Add(doacao);
+
+            Console.WriteLine("Doação cadastrada com sucesso.");
+
+            var estoque = estoquesDeSangue.FirstOrDefault(x =>
+                x.TipoSanguinio == doador.TipoSanguineo && x.FatorRh == doador.FatorRh);
+
+            if (estoque == null)
+            {
+                var novoEstoque = new EstoqueDeSangue
+                {
+                    Id = EstoqueId++,
+                    TipoSanguinio = doador.TipoSanguineo,
+                    FatorRh = doador.FatorRh,
+                    QuantidadeMl = doacao.QuantidadeMl
+                };
+
+                estoquesDeSangue.Add(novoEstoque);
+            }
+            else
+            {
+                estoque.QuantidadeMl += quantidadeML;
+            }
+        }
+
+        static void ConsultarDoador()
+        {
+            Console.WriteLine("Digite o email do doador: ");
+            string email = Console.ReadLine();
+
+            var doador = doadores.FirstOrDefault(x => x.Email == email);
+
+            if (doador is null)
+            {
+                Console.WriteLine("Doador não encontrado.");
+                return;
+            }
+
+            Console.WriteLine(
+                $"Nome: {doador.Nome} \n Tipo sanguineo: {doador.TipoSanguineo} \n Fator RH: {doador.FatorRh}");
+
+            if (doador.Doacoes.Any())
+            {
+                foreach (var doacao in doador.Doacoes)
+                {
+                     Console.WriteLine($"Data Doação: {doacao.DataDoacao}\n Quantidade: {doacao.QuantidadeMl}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhuma doação registrada.");
+            }
+
+
+        }
+    }
+    class Doador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Genero { get; set; }
+        public double Peso { get; set; }
+        public string TipoSanguineo { get; set; }
+        public string FatorRh { get; set; }
+        public List<Doacao> Doacoes { get; set; }
+
+    }
+
+    class Doacao
+    {
+        public int Id { get; set; }
+        public int IdDoador { get; set; }
+        public DateTime DataDoacao { get; set; } 
+        public int QuantidadeMl { get; set; }
+    }
+
+    class EstoqueDeSangue
+    {
+        public int Id {get; set; }
+        public string TipoSanguinio { get; set; }
+        public string FatorRh { get; set; }
+        public int QuantidadeMl { get; set; }
+    }
+    
+}
